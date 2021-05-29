01e48283ab76e68f7f64f8517d2b7a74cbd8cbc9
Kevin-Solomon <kevinsolomon404@gmail.com>
2021-05-29 15:55:54 +0530

    bug : empty userInput fixed

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const txtInput = document.querySelector(".txt-input");
 const txtOutput = document.querySelector(".txt-output");
 const button = document.querySelector("#btn-translate");
 
-const userInput = txtInput.value
-console.log(userInput)
-
+console.log(txtInput.value)
+function errorHandler(error){
+  console.log("error occured",error)
+  alert("Something went wrong woth the server")
+}
 function fetchValue(){
-  fetch('https://api.funtranslations.com/translate/minion.json?text=\'siek')
+  console.log(txtInput.value)
+  fetch(`https://api.funtranslations.com/translate/minion.json?text=${txtInput.value}`).then(response => response.json()).then(text => txtOutput.innerText = text.contents.translated).catch(errorHandler)
 }
 button.addEventListener("click", fetchValue)
